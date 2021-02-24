eccd642cee121764b14145a80defb89788e639a3
Diego <soterg.uhdeia@gmail.com>
2021-02-24 15:00:57 -0500

    responsive login added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "../css/login.css";
 import Logo from "../assets/logo.svg";
+import LogoRojo from "../assets/logorojo.svg";
+
 
 function Login() {
   return (
     <div className="container">
       <div className="bg" >
+        
         <section className="logo">
-          <img src={Logo} />
+          <img src={Logo} alt="logo"/>
         </section>
 
         <section className="login">
             <div className="login__container">
+              <img src={LogoRojo} alt="logo" />
                 <h3 className="login__title">Iniciar sesión</h3>
                 <input placeholder="Correo electrónico"/>
                 <input placeholder="Contraseña"/>
-                <span className="login__forgot">Olvidé la contraseña</span>
+                <span className="login__forgot"></span>
                 <button className="login__btn signin">INGRESAR</button>
                 <hr />
                 <button className="login__btn register">Crear Cuenta</button>
